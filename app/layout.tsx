24f37384a6d819f2e5ce97603f4358a8f6c5ad01
ahsan <makabir98@yahoo.com>
2023-05-18 17:58:15 +0600

    navbar completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { Nunito } from 'next/font/google'
+import Navbar from './components/navbar/Navbar'
 
 const font = Nunito({
   subsets :['latin'],
@@ -17,7 +18,10 @@ export const metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
