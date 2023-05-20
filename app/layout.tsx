15298f19a620cdca15e0731d1b937a7169a36ab3
ahsan <makabir98@yahoo.com>
2023-05-20 18:44:15 +0600

    login components and signout function done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import ClientOnly from './components/navbar/ClientOnly'
 import RegisterModal from './components/modals/RegisterModal'
 import ToasterProvider from './providers/ToasterProvider'
 import LoginModal from './components/modals/LoginModal'
+import getCurrentUser from './actions/getCurrentUser'
 
 const font = Nunito({
   subsets :['latin'],
@@ -19,7 +20,8 @@ export const metadata = {
   description: 'It is a clone of Airbnb',
 }
 
-export default function RootLayout({ children, }: { children: React.ReactNode }) {
+export default async function RootLayout({ children, }: { children: React.ReactNode }) {
+  const currentUser = await getCurrentUser();
   return (
     <html lang="en">
       <body className={font.className}>
@@ -27,7 +29,7 @@ export default function RootLayout({ children, }: { children: React.ReactNode })
           <ToasterProvider />
           <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser={currentUser}/>
         </ClientOnly>
         {children}
       </body>
